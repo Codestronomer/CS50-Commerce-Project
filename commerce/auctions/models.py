c5bfbead4b75f8609a175a8f8291450979514a3d
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-08-11 01:30:37 +0100

    added Bid, AuctionListings and comment to model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,20 +6,40 @@ class User(AbstractUser):
     pass
 
 
-class Listing(models.Model):
-    name = models.CharField(max_length=250)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bid')
+    amount = models.FloatField()
+
+    def __str__(self):
+        return f'Bid of {self.amount} made by {self.user}'
+
+
+class AuctionListing(models.Model):
+    item = models.CharField(max_length=250)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='auction_listings', blank=True)
     description = models.TextField()
-    slug = models.SlugField()
-    image = models.ImageField()
-    category = models.CharField(max_length=250)
+    image_url = models.CharField(max_length=300)
+    category = models.CharField(max_length=200)
     price = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
+    watchlist = models.ManyToManyField(User, blank=True, related_name='watch_listings')
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name='auction_listings', blank=True)
+    is_closed = models.BooleanField(default=False)
 
-
-class Bids(models.Model):
-    pass
+    def __str__(self):
+        return f'{self.item}: {self.bid}'
 
 
 class Comment(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name='comments')
+
+    class Meta:
+        ordering = ('created_on',)
+
+    def __str__(self):
+        return f'comment by {self.user} on {self.listing}'
